7ac095065244be387f3ffe75f522534f592ca600
JHONATANGARCIAXD <jhonatangarcia@colegioguanenta.edu.co>
2023-07-18 08:38:12 -0500

    Comentarios en PHP

---
+++ 1_Comentarios.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COMENTARIOS EN PHP</title>
+</head>
+<body>
+    <h1>COMENTARIOS EN PHP</h1>
+    
+    <!--FORMAS DE INCLUIR CODIGO PHP-->
+
+    <?php
+        //COMENTARIO DE UNA LINEA
+        /*ESTO ES UN COMENTARIO DE VARIAS LINEAS */ 
+    ?>
+
+
+    <!--
+    <%
+        codigo php
+    %>
+
+    <script language="php">
+
+    </script> 
+    -->
+    
+</body>
+</html>
